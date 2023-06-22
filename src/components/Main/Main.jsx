844c657791a8a712a5796d4210bce664191009a7
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-06-22 21:41:17 +0300

    create section AboutMe and element Portfolio

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,7 @@ import './Main.css'
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
+import AboutMe from "../AboutMe/AboutMe";
 
 function Main(props) {
   return (
@@ -10,6 +11,7 @@ function Main(props) {
       <Promo/>
       <AboutProject/>
       <Techs />
+      <AboutMe />
     </>
   );
 }
