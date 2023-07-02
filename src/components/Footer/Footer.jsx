b9a402f58611a8c1505525be265373f7bdfdbf63
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-07-02 23:11:51 +0300

    create Profile

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import './Footer.css'
+import {useLocation} from "react-router-dom";
+import {paths} from "../../utils/conts";
 
 function Footer(props) {
+  let location = useLocation()
+  let pathLocation = location.pathname
+
   return (
-    <footer className={'footer'}>
-      <h3 className={'footer__header'}>Учебный проект Яндекс.Практикум х BeatFilm.</h3>
-      <div className={'footer__container'}>
-        <p className="footer__text">© {new Date().getFullYear()}</p>
-        <ul className={'footer__list'}>
-          <li className={'footer__list-item'}>
-            <a className={'footer__link'} href="https://practicum.yandex.ru/">Яндекс.Практикум</a>
-          </li>
-          <li className={'footer__list-item'}>
-            <a className={'footer__link'} href="https://github.com/andrey-goryachev">Github</a>
-          </li>
-        </ul>
-      </div>
-    </footer>
+    <>
+      {(pathLocation === paths.main || pathLocation === paths.movies || pathLocation === paths.savedMovies) && <footer className={'footer'}>
+        <h3 className={'footer__header'}>Учебный проект Яндекс.Практикум х BeatFilm.</h3>
+        <div className={'footer__container'}>
+          <p className="footer__text">© {new Date().getFullYear()}</p>
+          <ul className={'footer__list'}>
+            <li className={'footer__list-item'}>
+              <a className={'footer__link'} href="https://practicum.yandex.ru/">Яндекс.Практикум</a>
+            </li>
+            <li className={'footer__list-item'}>
+              <a className={'footer__link'} href="https://github.com/andrey-goryachev">Github</a>
+            </li>
+          </ul>
+        </div>
+      </footer>}
+    </>
   );
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,25 @@ import './Profile.css'
 
 function Profile(props) {
   return (
-    <div>Profile</div>
+    <section className={'profile'}>
+      <h2 className={'profile__header'}>Привет, Виталий!</h2>
+      <form className={'profile__form'} name={'profile'}>
+        <div className={'profile__inputs'}>
+          <label className={'profile__label'}>
+            Имя
+            <input className={'profile__input'} type="text" placeholder={'Виталий'}/>
+          </label>
+          <label className={'profile__label'}>
+            E-mail
+            <input className={'profile__input'} type="email" placeholder={'pochta@yandex.ru'}/>
+          </label>
+        </div>
+        <div className={'profile__buttons'}>
+          <button className={'profile__button'}>Редактировать</button>
+          <button className={'profile__button'}>Выйти из аккаунта</button>
+        </div>
+      </form>
+    </section>
   );
 }
 
