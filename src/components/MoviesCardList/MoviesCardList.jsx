5a08c3ef65b2a6ad76313c94a8c6d314130c2ceb
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-07-12 21:58:19 +0300

    add changing the number of cards depending on the screen width

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -2,20 +2,33 @@ import React, {useEffect, useState} from 'react';
 import './MoviesCardList.css'
 import MoviesCard from "../MoviesCard/MoviesCard";
 
-// import Preloader from "../Preloader/Preloader";
-
 function MoviesCardList({movies, searchText}) {
-  const [width, setWidth] = useState(window.innerWidth);
-  const [newMoviesList, setNewMoviesList] = React.useState([])
+  const [width, setWidth] = useState(document.documentElement.clientWidth);
+  const [newMoviesList, setNewMoviesList] = useState([])
   const [maxMoviesPage, setMaxMoviesPage] = useState(0)
 
+  const breakpointDesktop = 1024;
   const breakpointTab = 768;
   const breakpointMobile = 460;
 
+  const increaseNumberMovies = () => {
+    if (width > breakpointDesktop) {
+      setMaxMoviesPage (maxMoviesPage + 4)
+    }
+    if (width <= breakpointDesktop && width > breakpointTab) {
+      setMaxMoviesPage (maxMoviesPage + 3)
+    }
+    if (width <= breakpointTab) {
+      setMaxMoviesPage (maxMoviesPage + 2)
+    }
+  }
 
-  useEffect(() => {
-    if (width > breakpointTab) {
+  const changeMaxMoviesPageDependingWidth = () => {
+    if (width > breakpointDesktop) {
       setMaxMoviesPage(16);
+    }
+    if (width <= breakpointDesktop && width > breakpointTab) {
+      setMaxMoviesPage(15);
     }
     if (width <= breakpointTab && width > breakpointMobile) {
       setMaxMoviesPage(8);
@@ -26,9 +39,27 @@ function MoviesCardList({movies, searchText}) {
     if (width === 0) {
       setMaxMoviesPage(0);
     }
+  }
 
+
+  useEffect(() => {
+    changeMaxMoviesPageDependingWidth()
+    const updateWidth = () => {
+      setWidth(document.documentElement.clientWidth)
+      changeMaxMoviesPageDependingWidth()
+    }
+    window.addEventListener('resize', updateWidth)
+    return () => {
+      window.removeEventListener('resize', updateWidth)
+    }
+  }, [])
+
+
+  useEffect(() => {
     setNewMoviesList(movies.slice(0, maxMoviesPage))
-  }, [movies]);
+    localStorage.setItem('moviesList', JSON.stringify(newMoviesList) )
+  }, [movies, maxMoviesPage]);
+
 
   return (
     <section className={'movies'}>
@@ -38,7 +69,7 @@ function MoviesCardList({movies, searchText}) {
         ))}
       </ul>
       {movies && (movies.length > maxMoviesPage) && <div className={'movies__button-container'}>
-        <button className={'button movies__button'} type={'button'}>Ещё</button>
+        <button className={'button movies__button'} type={'button'} onClick={increaseNumberMovies}>Ещё</button>
       </div>
       }
     </section>
